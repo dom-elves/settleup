0086cbd6ece45b9a913f17c642b47a874967fb51
Dom Elves <dom_elves@hotmail.co.uk>
2024-03-28 19:23:01 +0000

    mmcf for group & debt

--- database/migrations/2024_03_28_192217_create_groups_table.php
+++ database/migrations/2024_03_28_192217_create_groups_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('groups', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('password');
             $table->timestamps();
         });
     }
@@ -26,8 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('groups');
     }
 };
