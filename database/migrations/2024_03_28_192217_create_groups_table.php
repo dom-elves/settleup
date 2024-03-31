dc1f18746dc739c534b7bf021db36d71d66a72fd
Dom Elves <dom_elves@hotmail.co.uk>
2024-03-31 20:36:07 +0100

    add name column to group table

--- database/migrations/2024_03_28_192217_create_groups_table.php
+++ database/migrations/2024_03_28_192217_create_groups_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->json('user_ids');
+            $table->string('name');
             $table->float('kitty', 2);
             $table->timestamps();
         });
