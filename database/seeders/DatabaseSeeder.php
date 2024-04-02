2a95e760badba6396e03cb9d7d915eb80cc3824e
Dom Elves <dom_elves@hotmail.co.uk>
2024-04-02 17:17:58 +0100

    improve seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,20 +18,25 @@ class DatabaseSeeder extends Seeder
         $faker = Faker::create();
 
         User::factory()->create([
+            'first_name' => 'Dom',
+            'last_name' => 'Elves',
             'email' => 'dom_elves@hotmail.co.uk',
             'password' => 'password',
         ]);
 
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             User::factory()->create([
                 'first_name' => $faker->unique()->firstName,
                 'last_name' => $faker->unique()->country,
             ]);
         }
         
-        for ($i = 0; $i < 2; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             Group::factory()->create([
-                'user_ids' => json_encode(User::pluck('id')->shuffle()->take(5)->toArray()),
+                'user_ids' => json_encode(User::pluck('id')
+                    ->shuffle()
+                    ->take(random_int(2,10))
+                    ->toArray()),
             ]);
         }
 
