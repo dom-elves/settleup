1dea1b617acff21c7c2ee55d71f666a0ea3e035f
Dom Elves <dom_elves@hotmail.co.uk>
2024-04-03 17:45:12 +0100

    improve seeding to be a bit more random, add debts & self as a guarantee

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,25 +31,39 @@ class DatabaseSeeder extends Seeder
                 'last_name' => $faker->unique()->country,
             ]);
         }
+
+        $random = random_int(0, 10);
         
         for ($i = 0; $i < 10; $i++) {
+
+            // get between 2 and 10 user ids
+            $user_ids_array = User::pluck('id')
+                ->shuffle()
+                ->take(random_int(2,10))
+                ->toArray();
+            
+            // if $random matches and self not already in the ids array, add self
+            if ($random === $i && !in_array(1, $user_ids_array)) {
+                $user_ids_array[] = 1;
+                dump('adding self to group ' . $i );
+            } elseif (in_array(1, $user_ids_array)) {
+                // log if i am in the array, no real reason
+                dump('self in group ' . $i);
+            }
+
+            // column is json so ids array needs to be encoded
             $group = Group::factory()->create([
-                'user_ids' => json_encode(User::pluck('id')
-                    ->shuffle()
-                    ->take(random_int(2,10))
-                    ->toArray()),
+                'user_ids' => json_encode($user_ids_array),
             ]);
 
+            // generate a random decimal, not perfect
             $decimal = round(100 / random_int(100,1000), 2);
 
             Debt::factory()->create([
                 'group_id' => $group->id,
+                // add that decimal to a random integer to make it look like money
                 'amount' => random_int(1, 999) + $decimal,
             ]);
-        }
-
-
-
-        
+        }        
     }
 }
