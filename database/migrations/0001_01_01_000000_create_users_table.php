039ed5440ffafc8e409d845bbb5d5ff50d577faa
Dom Elves <dom_elves@hotmail.co.uk>
2024-04-29 21:46:48 +0100

    re-add old boilerplate so tests pass

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
 
