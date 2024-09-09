528159fed01cfc8fff4c2946fe430ff762f8f89c
Dom Elves <dom_elves@hotmail.co.uk>
2024-09-09 21:28:02 +0100

    add user debts to seeder, clean up other seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Group;
 use App\Models\Debt;
+use App\Models\UserDebt;
 use App\Models\GroupUser;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -22,12 +23,11 @@ class DatabaseSeeder extends Seeder
 	    $this->createGroups();
 		$this->createGroupUsers();
         $this->createDebts();
+        $this->createUserDebts();
     }
 
     public function createUsers()
     {
-        $faker = Faker::create();
-
         User::factory()->create([
             'first_name' => 'Dom',
             'last_name' => 'Elves',
@@ -36,24 +36,12 @@ class DatabaseSeeder extends Seeder
             'total_value' => 00.00,
         ]);
 
-        for ($i = 0; $i < 100; $i++) {
-            User::factory()->create([
-                'first_name' => $faker->unique()->firstName,
-                'last_name' => $faker->unique()->country,
-            ]);
-        }
+        User::factory(100)->create(); 
     }
 
     public function createGroups()
     {
-        $faker = Faker::create();
-	    
-        for ($i = 0; $i < 10; $i++) {
-            $group = Group::factory()->create([
-                // not perfect in terms of being unique but will do for now
-                'name' => $faker->word() . "_" . $faker->word(),
-            ]);     
-        }
+        $group = Group::factory(10)->create();     
     }
 
     public function createGroupUsers()
@@ -103,12 +91,14 @@ class DatabaseSeeder extends Seeder
             $total_amount = random_int(1,999) + round(100/random_int(100,1000), 2);
             $group_user = GroupUser::where('group_id', $group_id)->first();
             $user = User::findOrFail($group_user->user_id);
+
             Debt::create([
                 'group_id' => $group_id,
                 'name' => $faker->word(),
                 'total_amount' => $total_amount,
                 'group_user_id' => $group_user->user_id,
-                'split_even' => $faker->boolean(),
+                // todo: update this to not always split even, but find a way to randomly chunk debts
+                'split_even' => 1,
                 // todo: update this to eventually have some cleared debts
                 'cleared' => 0,
             ]);
@@ -116,5 +106,35 @@ class DatabaseSeeder extends Seeder
             dump("Debt added for group ${group_id} for ${total_amount} by " . $user->first_name . " " . $user->last_name);
         } 
     }
-}
+
+    public function createUserDebts()
+    {
+        $debts = Debt::all();
+        
+        foreach ($debts as $debt) {
+            // todo: refactor all this when relationships are written
+            $group_id = $debt->group_id;
+            $group_users = GroupUser::where('group_id', $group_id)->get();   
+            $split = $debt->total_amount / $group_users->count();
+            // this isn't perfect but will serve the purpose of messing around for now
+            $rounded_split = ceil($split * 100) / 100;
+            $formatted_split = number_format($rounded_split, 2);           
+            
+            foreach ($group_users as $group_user) {
+        
+                $paid = rand(0,1);
+
+                UserDebt::create([
+                   'user_id' => $group_user->id,
+                   'group_id' => $group_user->group_id,
+                   'debt_id' => $debt->id,
+                    // todo: update these as mentioned above, ranomly chunking debts
+                   'original_amount' => $formatted_split,
+                   'paid_amount' => $paid ? $formatted_split : 0,
+                   'cleared' => $paid ? 1 : 0, //for now we'll pretend all paid debts are cleared
+                ]);
+            }
+        }
+    }
+}   
  
