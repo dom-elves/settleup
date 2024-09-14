3d5da99cfeefeeeff8c773d6a4c2c336b7ec31e6
Dom Elves <dom_elves@hotmail.co.uk>
2024-09-14 22:43:29 +0100

    actually adding the blades

--- resources/views/livewire/groups/user_group.blade.php
+++ resources/views/livewire/groups/user_group.blade.php
@@ -1,8 +1,6 @@
 <div>
-    {{-- help
-    {{$groups}} --}}
-    {{-- <a href="{{ url('/group', ['name' => $group->name]) }}"><h4 class="text-xl">{{ $group->name }}</h4></a></br>
-    @foreach ($group->users as $user)
-        {{$user->first_name}} {{$user->last_name}}</br>
-    @endforeach --}}
+    {{-- todo: change this to route helper --}}
+    <a href="{{ url('/group', ['name' => $group->name]) }}"><h4 class="text-xl">{{ $group->name }}</h4></a></br>
+    {{ $group }}
+    {{-- after writing relationships, probably just fill this with user first names --}}
 </div>
